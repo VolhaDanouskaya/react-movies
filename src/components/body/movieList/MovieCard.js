9d4a702132ef0de82b400209426074355944dcc4
Volha_Danouskaya <volha.danouskaya@ukg.com>
2021-04-15 00:18:36 +0300

    Three dots menu added for movie card, scss refactoring

--- src/components/body/movieList/MovieCard.js
+++ src/components/body/movieList/MovieCard.js
@@ -1,8 +1,21 @@
 import "./movies.scss";
 import React from "react";
 import PropTypes from "prop-types";
+import IconButton from "@material-ui/core/IconButton";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
 const MovieCard = (props) => {
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <li className="movie-card">
       <img src={props.movie.poster_path} />
@@ -15,6 +28,28 @@ const MovieCard = (props) => {
         </div>
         <p className="genre">{props.movie.genres.join(", ")}</p>
       </div>
+      <IconButton
+        className="card-menu-icon"
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <MoreVertIcon />
+      </IconButton>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleClose} className="card-menu-item">
+            edit
+          </MenuItem>
+          <MenuItem onClick={handleClose} className="card-menu-item">
+            delete
+          </MenuItem>
+        </Menu>
     </li>
   );
 };
